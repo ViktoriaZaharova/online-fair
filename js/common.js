941985822cb432c7d52b255ef48466577be74cb6
Viktoria <zaharova181114@gmail.com>
2022-12-26 11:40:11 +0200

    add pc version index.html

--- js/common.js
+++ js/common.js
@@ -1,2 +1,26 @@
 $('[name="phone"]').mask('+7 (999) 999-99-99');
 
+$('.top-products-slider').slick({
+	slidesToShow: 4,
+	arrows: true,
+	infinite: false,
+	prevArrow: '<button type="button" class="slick-prev"><svg class="svg-icon"><use xlink:href="img/sprite.svg#next"></use></svg></button>',
+	nextArrow: '<button type="button" class="slick-next"><svg class="svg-icon"><use xlink:href="img/sprite.svg#next"></use></svg></button>',
+});
+
+$('.partners-slider').slick({
+	slidesToShow: 4,
+	arrows: true,
+	infinite: false,
+	prevArrow: '<button type="button" class="slick-prev"><svg class="svg-icon"><use xlink:href="img/sprite.svg#next"></use></svg></button>',
+	nextArrow: '<button type="button" class="slick-next"><svg class="svg-icon"><use xlink:href="img/sprite.svg#next"></use></svg></button>',
+});
+
+$('.new-manufacturers-slider').slick({
+	slidesToShow: 4,
+	arrows: true,
+	infinite: false,
+	prevArrow: '<button type="button" class="slick-prev"><svg class="svg-icon"><use xlink:href="img/sprite.svg#next"></use></svg></button>',
+	nextArrow: '<button type="button" class="slick-next"><svg class="svg-icon"><use xlink:href="img/sprite.svg#next"></use></svg></button>',
+});
+
