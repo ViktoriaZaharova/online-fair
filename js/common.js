58dd71f549bde48055f059b775001d03b42bc36e
Viktoria <zaharova181114@gmail.com>
2022-12-27 20:30:41 +0200

    index media

--- js/common.js
+++ js/common.js
@@ -44,6 +44,17 @@ $('.partners-slider').slick({
 	infinite: false,
 	prevArrow: '<button type="button" class="slick-prev"><svg class="svg-icon"><use xlink:href="img/sprite.svg#next"></use></svg></button>',
 	nextArrow: '<button type="button" class="slick-next"><svg class="svg-icon"><use xlink:href="img/sprite.svg#next"></use></svg></button>',
+	responsive: [
+		{
+			breakpoint: 992,
+			settings: {
+				slidesToShow: 1,
+				variableWidth: true,
+				appendArrows: '.partners-slider__nav',
+				infinite: false,
+			}
+		}
+	]
 });
 
 $('.new-manufacturers-slider').slick({
@@ -52,6 +63,17 @@ $('.new-manufacturers-slider').slick({
 	infinite: false,
 	prevArrow: '<button type="button" class="slick-prev"><svg class="svg-icon"><use xlink:href="img/sprite.svg#next"></use></svg></button>',
 	nextArrow: '<button type="button" class="slick-next"><svg class="svg-icon"><use xlink:href="img/sprite.svg#next"></use></svg></button>',
+	responsive: [
+		{
+			breakpoint: 992,
+			settings: {
+				slidesToShow: 1,
+				variableWidth: true,
+				appendArrows: '.new-manufacturers-slider__nav',
+				infinite: false,
+			}
+		}
+	]
 });
 
 $('.btn-burger').on('click', function () {
